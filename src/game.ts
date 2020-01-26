aa014b91dd319d54bc47380ef2d87516687aca2b
Ed <emundwilson@gmail.com>
2020-01-27 04:19:11 +1300

    Refactored JS to TS - Now Running

--- src/game.ts
+++ src/game.ts
@@ -1,1 +1,27 @@
 import 'phaser';
+import { GameScene } from './gameScene'
+
+const config: Phaser.Types.Core.GameConfig = {
+    title: 'BombDodge',
+    width: 800,
+    height: 600,
+    parent: 'game',
+    scene: [GameScene],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y: 450},
+            debug: false
+        }
+    }
+};
+
+export class BombDodgeGame extends Phaser.Game {
+    constructor(config: Phaser.Types.Core.GameConfig) {
+        super(config);
+    }
+}
+
+window.onload = () => {
+    let game = new BombDodgeGame(config);
+}
