0c40a1031417d464c8b80cf8e0c83e845a508ebd
Ed <emundwilson@gmail.com>
2020-02-03 17:14:42 +1300

    Added file loader for packaging assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const path = require('path');
 module.exports = {
   entry: './src/game.ts',
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/
-      }
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: 'file-loader'
+      },
     ]
   },
   resolve: {
