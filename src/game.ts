76dc5768332115814b98bfd5dad3839a12ab9e98
Ed <emundwilson@gmail.com>
2020-02-05 19:24:15 +1300

    Changed Formatting Rules, Event Driven Jumps

--- src/game.ts
+++ src/game.ts
@@ -3,25 +3,25 @@ import GameScene from './gameScene';
 import WelcomeScene from './welcomeScene';
 
 const config: Phaser.Types.Core.GameConfig = {
-  title: 'BombDodge',
-  width: 512,
-  height: 320,
-  parent: 'game',
-  pixelArt: true,
-  zoom: 3,
-  scene: [WelcomeScene, GameScene],
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 1000 },
-      debug: false,
+    title: 'BombDodge',
+    width: 512,
+    height: 320,
+    parent: 'game',
+    pixelArt: true,
+    zoom: 3,
+    scene: [WelcomeScene, GameScene],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 1000 },
+            debug: false,
+        },
     },
-  },
 };
 
 export default class BombDodgeGame extends Phaser.Game {}
 
 window.onload = () => {
-  // eslint-disable-next-line no-unused-vars
-  const game = new BombDodgeGame(config);
+    // eslint-disable-next-line no-unused-vars
+    const game = new BombDodgeGame(config);
 };
--- src/gameOverScene.ts
+++ src/gameOverScene.ts
@@ -1,34 +1,34 @@
 import * as Phaser from 'phaser';
 
 export default class GameOverScene extends Phaser.Scene {
-  score: number;
-  result: Phaser.GameObjects.Text;
-  hint: Phaser.GameObjects.Text;
-  constructor() {
-    super({
-      key: 'ScoreScene',
-    });
-  }
-  init(params: any): void {
-    this.score = params.starsCaught;
-  }
-  create(): void {
-    const resultText: string = `Your score is ${this.score}!`;
-    this.result = this.add.text(200, 250, resultText, {
-      font: '48px Arial Bold',
-      fill: '#FBFBAC',
-    });
-    const hintText: string = 'Any Key to Restart';
-    this.hint = this.add.text(300, 350, hintText, {
-      font: '24px Arial Bold',
-      fill: '#FBFBAC',
-    });
-    this.input.keyboard.on(
-      'keydown',
-      function(/* pointer */) {
-        this.scene.start('WelcomeScene');
-      },
-      this
-    );
-  }
+    score: number;
+    result: Phaser.GameObjects.Text;
+    hint: Phaser.GameObjects.Text;
+    constructor() {
+        super({
+            key: 'ScoreScene',
+        });
+    }
+    init(params: any): void {
+        this.score = params.starsCaught;
+    }
+    create(): void {
+        const resultText: string = `Your score is ${this.score}!`;
+        this.result = this.add.text(200, 250, resultText, {
+            font: '48px Arial Bold',
+            fill: '#FBFBAC',
+        });
+        const hintText: string = 'Any Key to Restart';
+        this.hint = this.add.text(300, 350, hintText, {
+            font: '24px Arial Bold',
+            fill: '#FBFBAC',
+        });
+        this.input.keyboard.on(
+            'keydown',
+            function(/* pointer */) {
+                this.scene.start('WelcomeScene');
+            },
+            this
+        );
+    }
 }
